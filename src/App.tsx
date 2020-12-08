170c7f4ef03638dd0c2822207dcc02de612cf01c
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-08 14:35:04 +0100

    Eksempel-komponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Panel from "nav-frontend-paneler";
+import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
+
+const situasjoner = ["arbeideledig", "i arbeid", "pensjonert", "på ferie", "NAV-ansatt", "sulten"];
+
+const fornavn = ["Julian", "Geir", "Michael", "Sergio", "Tormod"];
+const etternavn = ["Navnesen", "Testesen", "Olsen", "Hansen", "Navesen", "Narvesen"];
 
 function App() {
+  const situasjon = situasjoner[~~(situasjoner.length * Math.random())];
+  const navn = fornavn[~~(fornavn.length * Math.random())] + " " + etternavn[~~(etternavn.length * Math.random())];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>podlet-vta-situasjon er awsm!</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Panel border>
+        <Systemtittel>Hei, {navn}!</Systemtittel>
+        <Normaltekst>
+          Våre roboter har konkludert med at du er <b>{situasjon}</b>. Stemmer ikke dette?{" "}
+          <a href="mailto:toppledelsen@nav.no">Send klage.</a>
+        </Normaltekst>
+      </Panel>
     </div>
   );
 }
