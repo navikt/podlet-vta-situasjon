9dedae84591cc765c390a8fed53eb3ade98171f2
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-10 10:44:10 +0100

    Legger inn oppfølgingsdata

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,5 +1,6 @@
 import get from "./msw-utils";
 import oppfolging from "./oppfolging.json";
+import oppfolgingData from "./oppfolging-data.json";
 import meldekortinfo from "./meldekortinfo.json";
 import getNavn from "./person-navn";
 import ident from "./person-ident.json";
@@ -7,6 +8,7 @@ import innloggingslinje from "./innloggingslinje.json";
 
 export const handlers = [
   get("https://api.nav.no/dittnav-api/oppfolging", oppfolging),
+  get("https://api.nav.no/vta-api/oppfolging", oppfolgingData),
   get("https://api.nav.no/dittnav-api/meldekortinfo", meldekortinfo),
   get("https://api.nav.no/dittnav-api/personalia/navn", getNavn()),
   get("https://api.nav.no/dittnav-api/personalia/ident", ident),
