cb5f6c6e4d4d7aa65c8e649eb2d5d0346190c4b4
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-18 07:52:42 +0100

    Legger til mock for registrering

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,9 +2,11 @@ import get from "./msw-utils";
 import oppfolging from "./oppfolging.json";
 import meldekortinfo from "./meldekortinfo.json";
 import auth from "./auth.json";
+import registrering from "./registrering.json";
 
 export const handlers = [
   get("https://api.nav.no/vta-api/oppfolging", oppfolging),
   get("https://api.nav.no/dittnav-api/meldekortinfo", meldekortinfo),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
+  get("https://api.nav.no/vta-api/registrering", registrering),
 ];
