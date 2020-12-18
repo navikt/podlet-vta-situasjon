56f368700d129b82e02807343e702a8a15a44299
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-18 08:03:46 +0100

    Legger til henting av registrering dersom nivå 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Oppfolging from "./components/oppfolging";
 import Avregistrering from "./components/avregistrering";
 import Meldekort from "./components/meldekort";
 import Reaktivering from "./components/reaktivering";
-import { authUrl, meldekortinfoUrl, oppfolgingUrl } from "./url";
+import { authUrl, meldekortinfoUrl, oppfolgingUrl, registreringUrl } from "./url";
 
 const fetcher = async (url) => {
   const response = await fetch(url, { method: "GET", credentials: "include" });
@@ -17,13 +17,14 @@ function App() {
   const { data: auth } = useSWR(authUrl, fetcher);
   const { data: meldekort } = useSWR(meldekortinfoUrl, fetcher);
   const { data: oppfolging } = useSWR(auth && auth.securityLevel === "4" ? oppfolgingUrl : null, fetcher);
+  const { data: registrering } = useSWR(auth && auth.securityLevel === "4" ? registreringUrl : null, fetcher);
 
   return (
     <div className="podlet-vta-situasjon">
-      <Oppfolging oppfolging={oppfolging} />
+      <Oppfolging oppfolging={oppfolging} registrering={registrering} />
       <Meldekort meldekort={meldekort} />
-      <Avregistrering oppfolging={oppfolging} />
-      <Reaktivering oppfolging={oppfolging} />
+      <Avregistrering oppfolging={oppfolging} registrering={registrering} />
+      <Reaktivering oppfolging={oppfolging} registrering={registrering} />
     </div>
   );
 }
