aeb19640384e4d20addc7f1c5bb8fcedf94c54b9
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-18 11:29:02 +0100

    Lager sentry-release ved bygging
    Dette mapper opp source-maps til riktig versjon av koden, slik at vi enklere kan finne ut av hvor feil oppstår

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
   worker.start();
 } else {
-  Sentry.init({ dsn: "https://c24577bb13734aaeb8968748ec67a24f@sentry.gc.nav.no/59" });
+  Sentry.init({
+    dsn: "https://c24577bb13734aaeb8968748ec67a24f@sentry.gc.nav.no/59",
+    release: process.env.REACT_APP_SENTRY_RELEASE || "unknown",
+  });
 }
 
 ReactDOM.render(
