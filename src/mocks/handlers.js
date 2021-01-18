1d22902f683d4d1d60618aad1fb12751fccd0c4c
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-18 14:20:48 +0100

    Legger til meldekort på mock

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,14 @@
 import get from "./msw-utils";
 import oppfolging from "./oppfolging.json";
 import meldekortinfo from "./meldekortinfo.json";
+import meldekort from "./meldekort.json";
 import auth from "./auth.json";
 import registrering from "./registrering.json";
 
 export const handlers = [
   get("https://api.nav.no/vta-api/oppfolging", oppfolging),
   get("https://api.nav.no/dittnav-api/meldekortinfo", meldekortinfo),
+  get("https://api.nav.no/meldekort/meldekort", meldekort),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
   get("https://api.nav.no/vta-api/registrering", registrering),
 ];
