028946e7b6a41ebb874525b0e00c964ac6da18c5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-19 07:50:23 +0100

    Legger til dagerTilSisteFrist

---
+++ src/lib/meldekort-dager-til-siste-frist.js
@@ -0,0 +1,32 @@
+function arrify(input) {
+  if (!input) {
+    return [];
+  } else {
+    return Array.isArray(input) ? input : [input];
+  }
+}
+
+function getSisteFrist(datoStreng) {
+  const dato = new Date(datoStreng);
+  return dato.setDate(dato.getDate() + 7);
+}
+
+function dagerTilSisteFrist(meldekortHistorie) {
+  const iDag = new Date(new Date().toISOString().substr(0, 10));
+  let dager = false;
+  if (meldekortHistorie) {
+    const muligeMeldekort = arrify(meldekortHistorie.meldekort)
+      .filter((meldekort) => !meldekort.mottattDato)
+      .filter((meldekort) => new Date(meldekort.meldeperiode.kortKanSendesFra) < iDag)
+      .filter((meldekort) => getSisteFrist(meldekort.meldeperiode.til) > iDag);
+    if (muligeMeldekort.length === 1) {
+      const meldekort = muligeMeldekort[1];
+      const sisteFrist = getSisteFrist(meldekort.meldeperiode.til);
+      const dagerIgjen = Math.floor((sisteFrist - iDag) / (1000 * 3600 * 24));
+      dager = dagerIgjen;
+    }
+  }
+  return dager;
+}
+
+export default dagerTilSisteFrist;
