25d2c3f8c47f20ead031b3e82ba07b94f158d5da
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-01-21 13:07:36 +0100

    Disabler refetching med SWRConfig

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/browser";
 import "./index.css";
 import App from "./App";
+import { SWRConfig } from "swr";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
@@ -16,7 +17,9 @@ if (process.env.NODE_ENV === "development") {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig value={{ shouldRetryOnError: false }}>
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
   document.getElementById("podlet-vta-situasjon")
 );
