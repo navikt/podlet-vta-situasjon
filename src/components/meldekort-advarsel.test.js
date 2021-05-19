168bf585ec1bc0c5cab6e95cff7f2846f4228e77
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-05-19 11:14:11 +0200

    R liten endring på testene

--- src/components/meldekort-advarsel.test.js
+++ src/components/meldekort-advarsel.test.js
@@ -2,22 +2,9 @@ import { render } from "@testing-library/react";
 import MeldekortAdvarsel from "./meldekort-advarsel";
 
 describe("Tester komponenten MeldekortAdvarsel", () => {
-  test("Komponenten vises IKKE om man ikke har meldekort", () => {
-    const { container } = render(<MeldekortAdvarsel />);
-    expect(container).toBeEmptyDOMElement();
-  });
-
-  test("Komponenten vises IKKE om dager til frist ikke finnes", () => {
-    const frister = null;
-    const { container } = render(<MeldekortAdvarsel frister={frister} />);
-    expect(container).toBeEmptyDOMElement();
-  });
-
-  test("Komponenten vises IKKE om dagerTilFrist er under 0", () => {
-    const frister = {
-      dagerTilInaktivering: -1,
-    };
-    const { container } = render(<MeldekortAdvarsel frister={frister} />);
+  test("Komponenten vises IKKE om dagerEtterFastsattMeldedag til frist ikke finnes", () => {
+    const dagerEtterFastsattMeldedag = null;
+    const { container } = render(<MeldekortAdvarsel dagerEtterFastsattMeldedag={dagerEtterFastsattMeldedag} />);
     expect(container).toBeEmptyDOMElement();
   });
 
--- src/components/meldekort.test.js
+++ src/components/meldekort.test.js
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import Meldekort from "./meldekort";
+import MeldekortAdvarsel from "./meldekort-advarsel";
 
 describe("tester Meldekort komponenten", () => {
   test("komponenten rendres IKKE uten innhold", () => {
@@ -8,37 +9,8 @@ describe("tester Meldekort komponenten", () => {
     expect(container).toBeEmptyDOMElement();
   });
 
-  test("komponenten VISES med meldekortbruker true", () => {
-    const data = {
-      meldekortbruker: true,
-    };
-    const meldekorthistorie = {
-      maalformkode: "NO",
-      meldeform: "EMELD",
-      meldekort: [
-        {
-          meldekortId: 1526772064,
-          kortType: "ELEKTRONISK",
-          meldeperiode: {
-            fra: "2021-01-10T12:00:00+01:00",
-            til: "2021-01-24T12:00:00+01:00",
-            kortKanSendesFra: "2021-01-23T12:00:00+01:00",
-            kanKortSendes: false,
-            periodeKode: "202103",
-          },
-          meldegruppe: "DAGP",
-          kortStatus: "OPPRE",
-          bruttoBelop: 0.0,
-          erForskuddsPeriode: false,
-          korrigerbart: true,
-        },
-      ],
-    };
-    render(<Meldekort meldekortInfo={data} meldekortHistorie={meldekorthistorie} />);
-    expect(
-      screen.getByText(
-        /Det er innsending av meldekortet som opprettholder både statusen som arbeidssøker, samt brukes til å beregne/
-      )
-    ).toBeInTheDocument();
+  test("Komponenten vises IKKE om man ikke har meldekort", () => {
+    const { container } = render(<Meldekort />);
+    expect(container).toBeEmptyDOMElement();
   });
 });
